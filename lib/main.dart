74573efc1f22738cfded3cffdcd4bd5d242b2d42
Sutan Daffa Satria Hertanto <lagooo178@gmail.com>
2021-12-30 15:26:00 +0700

    Material Drawer & Listview

--- lib/main.dart
+++ lib/main.dart
@@ -16,36 +16,46 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Awesome App"),
       ),
-      body: Container(
-        width: 500,
-        color: Colors.teal,
-        child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Container(
-                padding: EdgeInsets.all(8),
-                width: 100,
-                height: 100,
-                color: Colors.red,
-                alignment: Alignment.center,
-              ),
-              Container(
-                padding: EdgeInsets.all(8),
-                width: 100,
-                height: 100,
-                color: Colors.yellow,
-                alignment: Alignment.center,
-              ),
-              Container(
-                padding: EdgeInsets.all(8),
-                width: 100,
-                height: 100,
-                color: Colors.green,
-                alignment: Alignment.center,
-              )
-            ]),
+      body: Center(
+        child: Container(
+          height: 100,
+          width: 100,
+          color: Colors.teal,
+        ),
       ),
+      drawer: Drawer(
+        child: ListView(
+          padding: const EdgeInsets.all(0),
+          children: <Widget>[
+            UserAccountsDrawerHeader(
+                accountName: Text("Daffa"),
+                accountEmail: Text("lagooo178@gmail.com"),
+                currentAccountPicture: CircleAvatar(
+                  backgroundImage: NetworkImage(
+                      "https://images.unsplash.com/photo-1555952517-2e8e729e0b44?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80"),
+                )),
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text("Daffa"),
+              subtitle: Text("Developer"),
+              trailing: Icon(Icons.edit),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: Icon(Icons.email),
+              title: Text("Email"),
+              subtitle: Text("lagooo178@gmail.com"),
+              trailing: Icon(Icons.edit),
+              onTap: () {},
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.edit),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
     );
   }
 }
